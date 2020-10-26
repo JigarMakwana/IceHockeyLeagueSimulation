3911f3fd5a9cd2d110d9e1801b6e4888bb0b843f
Harry Ben Alex <hr751368@dal.ca>
2020-10-27 01:26:32 +0530

    Generating Regular Schedule

--- src/main/java/group11/Hockey/models/SimulateSeason.java
+++ src/main/java/group11/Hockey/models/SimulateSeason.java
@@ -3,17 +3,27 @@ package group11.Hockey.models;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 
 public class SimulateSeason {
 
-	public SimulateSeason() {
-		
-	}
+	private HashMap<String,HashMap<Team,Team>> schedule;
 	
+	
+	
+
+	public SimulateSeason(HashMap<String, HashMap<Team, Team>> regularSchedule) {
+		this.schedule=regularSchedule;
+	}
+
+
+
 	public void StartSimulatingSeason(String date,String endDate) throws ParseException {	
 		    
 			Advance advance=new Advance();
 			Parse parseObj=new Parse();
+			CheckAndSimulateTodaySchedule simulateToday=new CheckAndSimulateTodaySchedule(schedule);
 			
 			Date dateTime=parseObj.parseStringToDate(date);		
 			Date endDateTime = parseObj.parseStringToDate(endDate);
@@ -33,7 +43,7 @@ public class SimulateSeason {
 				}
 				else{
 					//training(currentDate);
-					//new CheckAndSimulateTodaySchedule(date);
+					simulateToday.CheckAndSimulateToday(date);
 					if(dateTime.compareTo(tradeDeadLine)<=0){
 					//executeTrades();
 						//System.out.println("Entered Trades");

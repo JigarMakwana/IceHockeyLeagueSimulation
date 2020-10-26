4243ccb1e8dbe783be681bdc9a9e45110e183fe5
Harry Ben Alex <hr751368@dal.ca>
2020-10-26 21:06:12 +0530

    Introduce time concept

---
+++ src/main/java/group11/Hockey/models/CheckAndSimulateTodaySchedule.java
@@ -0,0 +1,9 @@
+package group11.Hockey.models;
+
+public class CheckAndSimulateTodaySchedule {
+
+	public void CheckAndSimulateToday(String date) {
+		
+	}
+
+}
---
+++ src/main/java/group11/Hockey/models/InitializeSeason.java
@@ -0,0 +1,53 @@
+package group11.Hockey.models;
+import java.text.ParseException;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAdjusters;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+
+public class InitializeSeason {	
+	
+	private int seasonCount;
+	
+	private League leagueObj;
+	
+	public InitializeSeason(League leagueObj) {
+		super();
+		this.leagueObj = leagueObj;
+	}
+
+	public int getSeasonCount() {
+		return seasonCount;
+	}
+	
+	public String startSeasons(int seasonCount) throws ParseException {
+		int year = Calendar.getInstance().get(Calendar.YEAR);
+		int count=seasonCount;
+		while(count>0) {
+			count--;			
+			String startDate="30/09/"+Integer.toString(year);			
+			year++;	
+			LocalDate eDate = LocalDate.of(year, Month.APRIL, 1);	
+		    LocalDate firstSaturday = eDate.with(TemporalAdjusters.firstInMonth(DayOfWeek.SATURDAY));
+		    String regularSeasonEndDate = firstSaturday.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+	
+			System.out.println("Start date : "+startDate); 
+			System.out.println("End date : "+regularSeasonEndDate); 			
+			
+			GenerateSeasonSchedule regularSeasonSchedule=new GenerateSeasonSchedule(startDate,regularSeasonEndDate,leagueObj);
+			HashMap<String,List<String>> regularSchedule=regularSeasonSchedule.getSeasonSchedule();
+			
+			Advance advanceObj=new Advance();
+			String regularSeasonStartDate=advanceObj.getAdvanceDate(startDate,1);
+			
+			SimulateSeason simulateSeason=new SimulateSeason();
+			simulateSeason.StartSimulatingSeason(regularSeasonStartDate,regularSeasonEndDate);
+		}
+		return seasonCount+" Seasons Simulated";
+	}
+
+}

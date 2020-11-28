ba94be5e9443b59c458aa6552af38bd42bdebf22
Harry Ben Alex <hr751368@dal.ca>
2020-11-28 22:23:40 +0530

    adding logs for defenseposition

--- src/main/java/group11/Hockey/BusinessLogic/DefensePosition.java
+++ src/main/java/group11/Hockey/BusinessLogic/DefensePosition.java
@@ -3,10 +3,14 @@
  */
 package group11.Hockey.BusinessLogic;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
 import group11.Hockey.BusinessLogic.models.IPlayer;
 
 public class DefensePosition implements IPlayerStrengthStrategy {
 	private IPlayer player;
+	private static Logger logger = LogManager.getLogger(DefensePosition.class);
 
 	public DefensePosition(IPlayer player) {
 		super();
@@ -15,6 +19,7 @@ public class DefensePosition implements IPlayerStrengthStrategy {
 
 	@Override
 	public float claculateStrength() {
+		logger.info("Entered claculateStrength()");
 		float playerStrength = player.getSkating() + player.getChecking() + (player.getShooting() / 2);
 		if (player.isInjured()) {
 			playerStrength = playerStrength / 2;

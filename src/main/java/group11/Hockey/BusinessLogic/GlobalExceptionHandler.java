e692809008bc9c5eb5e2de65087073016de43587
Harry Ben Alex <hr751368@dal.ca>
2020-12-01 01:49:59 +0530

    implemented global exception handler

---
+++ src/main/java/group11/Hockey/BusinessLogic/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package group11.Hockey.BusinessLogic;
+//Author: Harry B00856244
+
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+public class GlobalExceptionHandler implements Thread.UncaughtExceptionHandler, IGlobalExceptionHandler {
+		 
+		private static Logger logger = LogManager.getLogger(GlobalExceptionHandler.class);
+	 
+	    @Override
+		public void uncaughtException(Thread t, Throwable e) {
+	    	logger.error("Unhandled exception caught : "+e);
+	    }
+	}
---
+++ src/main/java/group11/Hockey/BusinessLogic/IGlobalExceptionHandler.java
@@ -0,0 +1,8 @@
+package group11.Hockey.BusinessLogic;
+
+
+public interface IGlobalExceptionHandler {
+
+	void uncaughtException(Thread t, Throwable e);
+
+}

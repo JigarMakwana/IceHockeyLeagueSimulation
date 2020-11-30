225f287a39bde597d5e87b9922a8d00b89992b9d
Harry Ben Alex <hr751368@dal.ca>
2020-11-30 21:30:57 +0530

    code cleaning

--- src/main/java/group11/Hockey/BusinessLogic/LeagueSimulation/Interfaces/ISimulateSeason.java
+++
@@ -1,6 +0,0 @@
-// Author: Harry B00856244
-package group11.Hockey.BusinessLogic.LeagueSimulation.Interfaces;
-
-public interface ISimulateSeason {
-	public boolean StartSimulatingSeason(String advanceDate,String date);
-}
